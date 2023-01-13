4160128585ac0eecff8a7f7189729739b570a6a7
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2023-01-13 15:44:40 +0200

    Service not registering

---
+++ src/main/java/com/example/application/data/service/CrmService.java
@@ -0,0 +1,51 @@
+package com.example.application.data.service;
+
+import com.example.application.data.entity.Contact;
+import com.example.application.data.repository.CompanyRepository;
+import com.example.application.data.repository.ContactRepository;
+import com.example.application.data.repository.StatusRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class CrmService {
+    private ContactRepository contactRepository;
+    private CompanyRepository companyRepository;
+    private StatusRepository statusRepository;
+
+    public CrmService(
+            ContactRepository contactRepository,
+            CompanyRepository companyRepository,
+            StatusRepository statusRepository
+    ) {
+        this.contactRepository = contactRepository;
+        this.companyRepository = companyRepository;
+        this.statusRepository = statusRepository;
+    }
+
+    public List<Contact> findAllContacts(String stringFilter) {
+        if(stringFilter == null || stringFilter.isEmpty()) {
+            return contactRepository.findAll();
+        } else {
+            return contactRepository.search(stringFilter);
+        }
+    }
+
+    public long countContacts() {
+        return contactRepository.count();
+    }
+
+    public void deleteContact(Contact contact) {
+        contactRepository.delete(contact);
+    }
+
+    public void saveContact(Contact contact) {
+        if (contact == null) {
+            System.err.println("Contact is null. Are you sure you have connected your form to the application?");
+            return;
+        }
+        contactRepository.save(contact);
+    }
+}
